959fa9464a33d9dd1b265f86d6d4c12f46f42a21
Harishshanmugamm <harishshanmugamm@outlook.com>
2024-09-21 18:34:12 +0530

    Fix: Adding constrains to Food Menu and Food Items

--- Models/FoodMenu.py
+++ Models/FoodMenu.py
@@ -1,6 +1,21 @@
 from AbstractItem import AbstractItem
+from FoodItem import FoodItem
 class FoodMenu(AbstractItem):
 
     def __init__(self,name):
         super().__init__(name)
-        self.FoodItems=[]
+        self.__FoodItems=[]
+
+    @property
+    def FoodItem(self):
+        return self.__FoodItems
+
+    @FoodItem.setter
+    def FoodItem(self,items):
+        for item in items:
+            if not isinstance(item,FoodItem):
+                print("Invalid FoodItem..")
+                return
+            self.__FoodItems = items
+
+
--- Models/Restaurant.py
+++ Models/Restaurant.py
@@ -1,8 +1,21 @@
 from AbstractItem import AbstractItem
+from FoodMenu import FoodMenu
 class Restaurant(AbstractItem):
 
     def __init__(self,name,rating,location,offer):
         super().__init__(name,rating)
         self.Location=location
         self.Offer=offer
-        self.FoodMenu=None
+        self.__FoodMenu=[]
+
+        @property
+        def FoodMenu(self):
+            return self.__FoodMenu
+
+        @FoodMenu.setter
+        def FoodMenu(self, items):
+            for item in items:
+                if not isinstance(item, FoodMenu):
+                    print("Invalid FoodMenu..")
+                    return
+                self.__FoodMenu = items
