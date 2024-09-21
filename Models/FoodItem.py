def3bb79b66f967d35adf5ee6e4763d5b20136c5
Harishshanmugamm <harishshanmugamm@outlook.com>
2024-09-21 10:51:11 +0530

    Fea: Models: Creating a FoodItem

---
+++ Models/FoodItem.py
@@ -0,0 +1,8 @@
+class FoodItem:
+
+    def __init__(self,name,rating,price,description):
+        self.Name=name
+        self.Rating = rating
+        self.Price = price
+        self.Description = description
+
